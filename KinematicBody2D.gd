6879aaeefd40ee3fbd381201e868d12c382ca7b1
flynn <flynnmeyer1@gmail.com>
2019-03-01 22:10:20 -0800

    a bunch of stuff

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -5,7 +5,11 @@ var motion = Vector2()
 var most_recent = 'none'
 var animation_state = IDLE
 var jumping = false
-var speed = 250
+var speed = 300
+var audio_player = null
+var have_jump_upgrade = false
+
+
 
 enum {
 	JUMP
@@ -15,23 +19,36 @@ enum {
 }
 
 
+#func _ready():
+#	var plr = AudioStreamPlayer.new()
+#
+#	plr.connect("finished", KinematicBody2D, "_audio_finished")
+#	self.add_child(plr)
+#	plr.stream = load('res://untitled.ogg')
+#	audio_player = plr
+
+	
+func sound_affect(affect):
+	audio_player.play()
+
 func _physics_process(delta):
-	motion.y +=  10
+	motion.y +=  24
+		
+		
 	
 	var right = Input.is_action_pressed('ui_right')
 	var left  = Input.is_action_pressed('ui_left')
-	
-	
+
+
 	if Input.is_action_just_pressed('ui_right'):
 		most_recent = 'right'
 	if Input.is_action_just_pressed('ui_left'):
 		most_recent = 'left'
-		
-	
+
+
 	if left:
 		go_left()
 	if right:
-		print('  right')
 		go_right()
 	if left && right:
 		if most_recent == 'right':
@@ -41,19 +58,21 @@ func _physics_process(delta):
 	if !(left || right):
 		idle()
 	if is_on_floor():
+#		audio_player.stop()
 		jumping = false
 		if Input.is_action_pressed('ui_up'):
-			motion.y = -300
+			motion.y = player_data.jump_height
+		#	audio_player.play()
 			jumping = true
 	else:
 		if jumping:
 			animation_state = JUMP
 		else:
 			animation_state = FALL
-	
-	
+
+
 	motion = move_and_slide(motion, UP)
-	
+
 	match animation_state:
 		IDLE:
 			$Sprite.play('Idle')
